40075fbd916bfe399e13398b2b0ed113073d7500
tomashrib <tomas.hrib@proton.me>
2023-03-22 12:00:17 +0100

    Simulate slow response due to single-threaded nature of server

--- app/src/main.rs
+++ app/src/main.rs
@@ -2,6 +2,8 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
 fn main() {
@@ -18,13 +20,15 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = 
-        if request_line == "GET / HTTP/1.1" {
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
             ("HTTP/1.1 200 OK", "index.html")
-        } else {
-            ("HTTP/1.1 404 NOT FOUND", "404.html")
         }
-    ;
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    };
+    
 
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
